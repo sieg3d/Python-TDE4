a22859737b97b82c34267d66f4f174aee01325fd
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2023-08-29 00:58:49 -0300

    Questão 6 owned
    #6. Escreva uma função que recebe como entrada uma lista ordenada de números e retorna o índice do primeiro elemento maior que um elemento limite.
    Se nenhum elemento da lista for maior que o limite desejado, retorne o valor -1.

--- tde4.py
+++ tde4.py
@@ -55,6 +55,7 @@ print('Quantidade de números maiores que limite:', conta_se_maior(a, b, limite)
 
 #5. Escreva uma função que, dado um número nota representando a nota de um estudante, converte o valor de nota para um conceito (A, B, C, D, E e F).
 
+'''
 def conceito(nota):
     if nota >= 9:
         return 'A (Excelente!)'
@@ -73,3 +74,26 @@ def conceito(nota):
 nota = float(input('Insira a nota do aluno: '))
 
 print(f'A nota {nota} convertida em conceito é:', conceito(nota))
+'''
+
+#6. Escreva uma função que recebe como entrada uma lista ordenada de números e retorna o índice do primeiro elemento maior que um elemento limite.
+#Se nenhum elemento da lista for maior que o limite desejado, retorne o valor -1.
+
+def maior_elemento(lista, limite):
+    posicao = None
+    for valor in lista:
+        if valor > limite:
+            posicao = lista.index(valor)
+            break
+        else:
+            posicao = -1
+
+    return posicao
+
+
+lista = [10,20,30,40,50,60,70,80,90]
+limite = float(input('Digite o elemento limite: '))
+maior_elemento(lista, limite)
+
+
+print(maior_elemento(lista, limite))
