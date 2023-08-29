0ab3e7024b10ec63bd1e94f6278df344ea1e786d
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2023-08-29 00:00:38 -0300

    Questão 3 owned
    #3. Escreva uma função que recebe dois números (a e b) como parâmetro e retorne True caso a soma dos dois seja maior que um terceiro parâmetro, chamado limite.

--- tde4.py
+++ tde4.py
@@ -20,3 +20,14 @@ numero = float(input("Digite um número: "))
 valor = valor_absoluto(numero)
 print(f'O valor absoluto do numero {numero} é {valor}.')
 '''
+
+#3. Escreva uma função que recebe dois números (a e b) como parâmetro e retorne True caso a soma dos dois seja maior que um terceiro parâmetro, chamado limite.
+
+def limitador(a, b, limite):
+    return a + b > limite
+        
+a = float(input('Digite o valor de A: '))
+b = float(input('Digite o valor de B: '))
+limite = float(input('Digite o limite: '))
+
+print(limitador(a, b, limite))
