0bb2952fec286d7fdc0cebc550b47b45dbce5aaf
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2023-08-28 23:47:43 -0300

    Questao 2 owned
    #2. Escreva uma função para imprimir o valor absoluto de um número.

--- tde4.py
+++ tde4.py
@@ -1,5 +1,5 @@
 #1.Escreva uma função que recebe um número n como parâmetro e imprime se n é positivo ou negativo.
-
+'''
 def pos_ou_neg(n):
     if n < 0:
         print(f'{n} é um número negativo.')
@@ -9,4 +9,14 @@ def pos_ou_neg(n):
               
 n = float(input('Digite um número: '))
 pos_ou_neg(n)
+'''
 
+#2. Escreva uma função para imprimir o valor absoluto de um número.
+'''
+def valor_absoluto(numero):
+    return abs(numero)
+
+numero = float(input("Digite um número: "))
+valor = valor_absoluto(numero)
+print(f'O valor absoluto do numero {numero} é {valor}.')
+'''
